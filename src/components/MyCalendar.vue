d37421619a4dd6f2494ab1a8a2f8dfd4729e23d6
Diether Dorado <xminustwothreex@gmail.com>
2025-07-11 14:13:42 -0700

    Fix timezone

--- src/components/MyCalendar.vue
+++ src/components/MyCalendar.vue
@@ -803,7 +803,13 @@ export default {
             const duration = service === 'KAP Dosing' ? 180 : 50;
             const endDate = new Date(startDate.getTime() + duration * 60000);
 
-            const formatted = endDate.toISOString().slice(0, 16);
+            const pad = (n) => n.toString().padStart(2, '0');
+            const formatted = `${endDate.getFullYear()}-
+                               ${pad(endDate.getMonth() + 1)}-
+                               ${pad(endDate.getDate())}T
+                               ${pad(endDate.getHours())}:
+                               ${pad(endDate.getMinutes())}`;
+                               
             this.form.end = formatted;
         }
     },
