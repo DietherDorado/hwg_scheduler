71665bc232cf16a69dae461466e73357d1d605b6
Diether Dorado <xminustwothreex@gmail.com>
2025-07-14 11:25:31 -0700

    Fix

--- src/components/MyCalendar.vue
+++ src/components/MyCalendar.vue
@@ -149,8 +149,8 @@ export default {
             .then(data => {
                 this.allEvents = data.map(event => ({
                     ...event,
-                    start: new Date(event.start),
-                    end: new Date(event.end)
+                    start: new Date(event.timestart),
+                    end: new Date(event.timeend)
                 }))
             })
             .catch(err => {
