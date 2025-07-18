4229545422cc31d2c76611278d7f801c5d3bdba5
Diether Dorado <xminustwothreex@gmail.com>
2025-07-18 11:55:32 -0700

    Revert back to timeGridView

--- src/components/MyCalendar.vue
+++ src/components/MyCalendar.vue
@@ -96,7 +96,7 @@ export default {
                 height: 'auto',
                 themeSystem: 'bootstrap5',
                 plugins: [dayGridPlugin, timeGridPlugin, listPlugin, BootstrapTheme],
-                initialView: 'dayGridMonth',
+                initialView: 'timeGridWeek',
                 views: {
                     timeGridWeek: {
                         slotMinTime: '07:00:00',
